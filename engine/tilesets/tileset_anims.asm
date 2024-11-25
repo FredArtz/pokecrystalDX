ea8dcd80bde9abb625a69d5d9b9af479435b21a2
Fredzbenedetti <133259693+Fredzbenedetti@users.noreply.github.com>
2024-11-25 14:19:44 -0300

    Initial Changes
    - 255 Tiles Upgrade
    - Tileset Attributes Upgrade
    - New Bark Town Tileset

--- engine/tilesets/tileset_anims.asm
+++ engine/tilesets/tileset_anims.asm
@@ -275,6 +275,7 @@ TilesetHoOhWordRoomAnim:
 TilesetKabutoWordRoomAnim:
 TilesetOmanyteWordRoomAnim:
 TilesetAerodactylWordRoomAnim:
+TilesetNewBarkTownAnim:
 	dw NULL,  WaitTileAnimation
 	dw NULL,  WaitTileAnimation
 	dw NULL,  WaitTileAnimation
